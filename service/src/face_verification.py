dca8f155049fa5e8e545253f8b7b74379763a50f
zzmalika <malika.zhusupbaeva@gmail.com>
2020-12-06 06:42:41 +0600

    fast face_recognition

--- service/src/face_verification.py
+++ service/src/face_verification.py
@@ -1,60 +1,39 @@
-print("start")
-import face_recognition
-import cv2
-import numpy
-import os
 from skimage import io
+import dlib
+import os
+from scipy.spatial import distance
 
 
-def face_verification(): #face_id, face_check
-    tolerance = 0.6
-    MODEL = 'cnn'
-    face_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+def get_description(path):
+    shape_rec = dlib.shape_predictor('service/src/models/shape_predictor_68_face_landmarks.dat')
+    face_rec = dlib.face_recognition_model_v1('service/src/models/dlib_face_recognition_resnet_model_v1.dat')
+    detector = dlib.get_frontal_face_detector()
+    image = io.imread(path)
+    captured_rectangle = detector(image, 1)
+    biggest_face = {}
+    for k, d in enumerate(captured_rectangle):
+        shape_res = shape_rec(image, d)
+
+        square_rectangle = (d.right() - d.left()) * (d.bottom() - d.top())
+        biggest_face[shape_res] = square_rectangle
+    shape_result = max(biggest_face, key=biggest_face.get)
+    face_descriptor = face_rec.compute_face_descriptor(image, shape_result)
+    return face_descriptor
+
+
+def face_verification():
+    print(os.getcwd())
     face_id_path = 'service/src/images/img_card/'
     face_check_path = 'service/src/images/img_selfie/'
-    crop_id_photo = 'service/src/images/id_photo_crop.jpg'
 
     face_id_to = os.listdir(face_id_path)
     face_id_to.sort(key=lambda x: os.path.getmtime(face_id_path + x))
     face_id = face_id_to[-1]
-    print(face_id)
     face_check_to = os.listdir(face_check_path)
     face_check_to.sort(key=lambda x: os.path.getmtime(face_check_path + x))
     face_check = face_check_to[-1]
-    img_id = io.imread(face_id_path + face_id)
-    faces = face_cascade_db.detectMultiScale(img_id, 1.1, 19)
-    img_id_gray = cv2.cvtColor(img_id, cv2.COLOR_BGRA2GRAY)
-    for (x, y, w, h) in faces:
-        left = 10
-        right = 10
-        top = 10
-        bottom = 10
 
-        cv2.rectangle(img_id, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        img_id = img_id_gray[y - top:y + h + bottom, x - left:x + w + right]
-    cv2.imwrite(crop_id_photo, img_id)
-
-    img_id_crop = face_recognition.load_image_file(face_check_path + face_check)
-    encoded_face_id = face_recognition.face_encodings(img_id_crop)[0]
-    # ---------------------
-    img_check = face_recognition.load_image_file(face_check_path + face_check)
-
-    face_location = face_recognition. face_locations(img_check, model=MODEL)
-    # face_location = [(371, 646, 878, 139)]
-    encoded_face_check = face_recognition.face_encodings(img_check, face_location)
-    result = face_recognition.compare_faces(encoded_face_id, encoded_face_check, tolerance)
-
-    return result[0]
-
-
-
-
-
-
-
-
-
-
-
-
-
+    id_description = get_description(face_id_path + face_id)
+    check_description = get_description(face_check_path + face_check)
+    comparison = distance.euclidean(id_description, check_description)
+    return str(round(100 - comparison, 2)) + '%'
